c4a325cac2aa940fb51e06622614b15f294581a4
Matheus de Almeida <69125506+matheusmra@users.noreply.github.com>
2024-12-15 21:42:57 -0300

    Update ex01.java

--- Teste/src/ex01.java
+++ Teste/src/ex01.java
@@ -1,8 +1,8 @@
 
 public class ex01 {
     public static void main(String[] args) {
-        System.out.printf("Primeiro código em Java!");
-
+        int x = 10;
+        System.out.println("Primeiro código em Java!");
         for (int i = 1; i <= 5; i++ ){
             System.out.println("i = " + i);
             if(i %2 == 0){
@@ -11,5 +11,6 @@ public class ex01 {
                 System.out.println("O numero: " + i + " é impar");
             }
         }
+        System.out.print(x);
     }
 }
