3b71095652a660bb101bf0382bf8e71f6b242c35
Matheus de Almeida <69125506+matheusmra@users.noreply.github.com>
2024-12-18 18:03:34 -0300

    Create ed02.java

---
+++ Teste/src/ed02.java
@@ -0,0 +1,89 @@
+import java.util.Scanner;
+public class ed02 {
+    public static boolean isPar(int numero){
+        if(numero %2 == 0){
+            return true;
+        }
+        return false;
+    }
+    public static void ex0211(Scanner ler) {
+        System.out.print("Digite o valor do numero:");
+        int x = ler.nextInt();
+        if(x > 0) {
+            if (isPar(x)) {
+                System.out.print("O numero: " + x + " é par!");
+            } else {
+                System.out.print("O numero é impar");
+            }
+        }else{
+            System.out.print("O numero digitado é negativo");
+        }
+    }
+    public static void ex0212(Scanner ler) {
+        System.out.print("Digite o valor do número: ");
+        int x = ler.nextInt();
+
+        if (!isPar(x) && x < -15) {
+            System.out.println("O número é ímpar e menor que -15.");
+        } else if (isPar(x) && x > 15) {
+            System.out.println("O número é par e maior que 15.");
+        } else if (!isPar(x)) {
+            System.out.println("O número é ímpar, mas não é menor que -15.");
+        } else {
+            System.out.println("O número é par, mas não é maior que 15.");
+        }
+    }
+    public static boolean isIntervalo(int numero){
+        if(numero >= 25 && numero <= 45){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    public static void ex0213(Scanner ler){
+        System.out.println("Numero =");
+        int x = ler.nextInt();
+        if(isIntervalo(x)){
+            System.out.println("O numero esta no intervalo entre 25 e 45");
+        }else{
+            System.out.println("O numero nao esta no intervalo");
+        }
+    }
+
+    public static boolean isIntervalo2(int numero){
+        if(numero > 20 && numero < 60)
+    }
+
+    public static void main(String[] args) {
+        int opcao;
+        do {
+
+            System.out.print("\nOpcao = ");
+            Scanner ler = new Scanner(System.in);
+            opcao = ler.nextInt();
+
+            // Executar a opção escolhida
+            switch (opcao) {
+                case 0:
+                    break;
+                case 1:
+                    ex0211(ler);
+                    break;
+                case 2:
+                    ex0212(ler);
+                    break;
+                case 3:
+                    ex0213(ler);
+                    break;
+                default:
+                    System.out.print("\nERRO: OPCAO INVALIDA\n\n");
+                    break;
+            } // fim switch
+
+        } while (opcao != 0);
+
+        System.out.print("\nAperte ENTER para terminar!\n");
+
+    }
+}
+
