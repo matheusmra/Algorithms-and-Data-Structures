3d598da4cd3bdcb56d0152f5680e38927d6b4cbc
Matheus de Almeida <69125506+matheusmra@users.noreply.github.com>
2025-02-14 17:55:55 -0300

    Create pesquisasequencial.java

---
+++ Estudos/QUIZ03/pesquisasequencial.java
@@ -0,0 +1,23 @@
+package Estudos.QUIZ03;
+import java.util.Scanner;
+public class pesquisasequencial {
+    public static boolean PesquisaSequencial(int[] vetor, int valor){
+        for(int i = 0; i < vetor.length; i++){
+            if(vetor[i] == valor){
+                i = vetor.length;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args){
+        int[] vetor = {1,2,3,4,5,6,7,8,9,10};
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Digite o valor a ser pesquisado: ");
+        int valor = sc.nextInt();
+        if(PesquisaSequencial(vetor, valor)){
+            System.out.println("Valor encontrado!");
+    }
+}
+}
