49063d218da602e72cfe8b5c2f387be1c8e39631
Matheus de Almeida <69125506+matheusmra@users.noreply.github.com>
2025-02-21 12:53:40 -0300

    Create string_invertida.java

---
+++ TPS/01/string_invertida.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class string_invertida {
+    /**
+     * Função para inverter uma string
+     * @return str2 - String invertida
+     * @param str - String a ser invertida
+     */
+    public static String inverter(String str) {
+        String str2 = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            str2 += str.charAt(i);
+        }
+        return str2;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String strInvertida = inverter(str);
+        System.out.println(strInvertida);
+        sc.close();
+        }
+    }
+    
+
