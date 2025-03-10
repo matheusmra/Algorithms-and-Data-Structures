9fb0c636aaaa11a96b8449f3a806e42d8cd09a4c
Matheus de Almeida <69125506+matheusmra@users.noreply.github.com>
2025-03-10 20:29:27 -0300

    Update soma_rec.c

--- TPS/01/EX22/soma_rec.c
+++ TPS/01/EX22/soma_rec.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#define max_size 100
+#define TAM_MAX 100
+    /**
+     * Função recursiva para obter a soma dos digitos
+     * @return soma dos digitos
+     * @param n - Numero que terá seus digitos somados
+     */
+
 int soma_digitos(int n)
 {
-    if(n == 0)
+    if(n == 0) // Caso para encerrar a recursão
     {
         return 0;
     }
     else
     {
-        return n % 10 + soma_digitos(n/10);
+        return n % 10 + soma_digitos(n/10); 
     }
 
 }
@@ -18,7 +24,7 @@ int soma_digitos(int n)
 int main()
 {
     int n = 0;
-    char string[max_size];
+    char string[TAM_MAX];
     while (scanf("%s", string) == 1 && strcmp(string, "FIM") != 0) {
         int n = atoi(string);
         printf("%d\n", soma_digitos(n));
