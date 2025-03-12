057dc8518111bfe889170df71d70bf473dc5c2fe
Matheus de Almeida <69125506+matheusmra@users.noreply.github.com>
2025-03-12 18:12:40 -0300

    Update AlteracaoAleatoria.java

--- TPS/01/EX04/AlteracaoAleatoria.java
+++ TPS/01/EX04/AlteracaoAleatoria.java
@@ -1,28 +1,43 @@
+
+
 import java.util.Random;
 import java.util.Scanner;
 
-public class AlteracaoAleatoria {    
-    public static String swap_Letters(String input, Random gerador) {
-        char letra1 = (char) ('a' + (Math.abs(gerador.nextInt()) % 26));
-        char letra2 = (char) ('a' + (Math.abs(gerador.nextInt()) % 26)); 
-        String resultado = "";
-        for (int i = 0; i < input.length(); i++) {
-            if (input.charAt(i) == letra1) {
-                resultado += letra2;
-            } else {
-                resultado += input.charAt(i);
+public class AlteracaoAleatoria {
+        /**
+     * Função para alterar uma string aleatoriamente.
+     * @param str String a ser alterada
+     * @param caracter Letra sorteada para ser substituida
+     * @param sub Letra sorteada para substituir a primeira
+     * @return "SIM" se forem anagramas, senão "NÃO"
+     */
+    public static String swap_Letters(String str, char caracter, char sub){
+        String result = " ";
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) != caracter) {
+                result = result + str.charAt(i); // Armazena o caractere na string
+            }else{
+                result = result + sub; // Realiza a substituição e armazena na string
             }
         }
-        return resultado;
+        return result;
     }
+    
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        char caracter = ' ';
+        char sub = ' ';
         Random gerador = new Random();
-        String str = "";
         gerador.setSeed(4);
-        while (!(str = sc.nextLine()).equals("FIM")) {;
-            System.out.println(swap_Letters(str,gerador));
-            }
-            sc.close();
+        Scanner sc = new Scanner(System.in);
+        String str = "";
+        String str2 = "";
+        while (!(str = sc.nextLine()).equals("FIM")) {
+            caracter  =  ( char ) ( 'a' + ( Math.abs( gerador.nextInt( ) ) % 26 ) );
+            sub = ( char ) ( 'a' + ( Math.abs( gerador.nextInt( ) ) % 26 ) );
+            str2 = swap_Letters(str, caracter, sub);
+            System.out.println(str2);
+        }
+        sc.close();
     }
+    
 }
