d8de601a5f43f3811649a75171ed0d5afcf81ecf
Matheus de Almeida <69125506+matheusmra@users.noreply.github.com>
2025-03-12 17:55:18 -0300

    Update isanagrama.java

--- TPS/01/EX09/isanagrama.java
+++ TPS/01/EX09/isanagrama.java
@@ -8,36 +8,37 @@ import java.util.Scanner;
 
 public class isanagrama {
     /**
-     *  Funcao para identificar se duas strings são anagramas
-     *  @return verdadeiro ou falso
-     *  @param str1 String
-     *  @param str2 String
-    */
-    public static boolean isanagram(String str1, String str2) {
-        int[] soma = new int[256];
-        if (str1.length() != str2.length()) { // Se o tamanho das duas strings for diferente o programa irá retornar falso
+     * Função para identificar se duas palavras são anagramas.
+     * @param str1 Primeira palavra
+     * @param str2 Segunda palavra
+     * @return "SIM" se forem anagramas, senão "NÃO"
+     */
+    public static boolean isanagrama(String str1, String str2) {
+        if (str1.length() != str2.length()) {
             return false;
-        }
+        } //Caso o tamanho das duas strings o programa retorna falso
+        int[] freq = new int[128]; // Array de tamanho 128 para verificar todas as ocorrências possiveis de um caractere (ASCII)
         for (int i = 0; i < str1.length(); i++) {
-                soma[str1.charAt(i)]++; 
-                soma[str2.charAt(i)]--;
+            freq[str1.charAt(i)]++;
+            freq[str2.charAt(i)]--;
+        }
+        // Loop para verificar se é um anagrama
+        for (int i = 0; i < 128; i++) {
+            if (freq[i] != 0) { // Caso o resultado no array seja diferente de 0, significa que não é um anagrama
+                return false;
             }
-            for(int i = 0; i < 256; i++){
-                if(soma[i] != 0){
-                    return false;
-                }
-            }
+        }
         return true;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String str1;
-        String str2;
-        while (!(str1 = sc.nextLine()).equals("FIM")) {
-            str2 = sc.nextLine();
-        System.out.println(isanagram(str1,str2) ? "SIM" : "NÃO");
+        String str;
+        while (!(str = sc.nextLine()).equals("FIM")) {
+            String[] palavras = str.split(" - "); // Retira o (espaço)-(espaço), seprando as strings
+            System.out.println(isanagrama(palavras[0].toLowerCase(), palavras[1].toLowerCase()) ? "SIM" : "NÃO");
+            // A função toLowerCase foi utilizada para evitar com que o programa retornasse Não quando as strings fossem anagramas porém com diferentes letras em maiúsculo
+        }
+        sc.close();
     }
-    sc.close();
-}
 }
