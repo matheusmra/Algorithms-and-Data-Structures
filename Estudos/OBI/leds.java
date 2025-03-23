be0dbf0873b54552cb32f17d74ca844037c8a516
Matheus de Almeida <69125506+matheusmra@users.noreply.github.com>
2025-03-23 14:21:18 -0300

    Update leds.java

--- Estudos/OBI/leds.java
+++ Estudos/OBI/leds.java
@@ -5,21 +5,19 @@ public class leds {
         Scanner sc = new Scanner(System.in);
         
         int n = sc.nextInt();
-        sc.nextLine(); // Consumir a quebra de linha após o nextInt()
+        sc.nextLine(); 
 
         String[] num = new String[n];
         for (int i = 0; i < n; i++) {
-            num[i] = sc.nextLine(); // Ler cada número como string
+            num[i] = sc.nextLine(); 
         }
-
-        // Mapeamento de LEDs por dígito
         int[] ledsPorDigito = {6, 2, 5, 5, 4, 5, 6, 3, 7, 6}; // 0-9
 
         for (int i = 0; i < n; i++) {
             int total = 0;
             for (int j = 0; j < num[i].length(); j++) {
                 char ch = num[i].charAt(j);
-                total += ledsPorDigito[ch - '0']; // Converter char para índice do array
+                total += ledsPorDigito[ch - '0']; 
             }
             System.out.println(total + " leds");
         }
