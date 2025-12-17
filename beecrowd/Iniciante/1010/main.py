4050b0d6deb2e60a308d443d56b3ee4b8062ed69
Matheus de Almeida <69125506+matheusmra@users.noreply.github.com>
2025-12-16 22:16:54 -0300

    +3 questions

---
+++ beecrowd/Iniciante/1010/main.py
@@ -0,0 +1,4 @@
+a, b, c = input().split()
+d, e, f = input().split()
+total = int(b) * float(c) + int(e) * float(f)
+print(f"VALOR A PAGAR: R$ {total:.2f}")
---
+++ beecrowd/Iniciante/1011/main.py
@@ -0,0 +1,4 @@
+pi = 3.14159
+r = int(input())
+volume = float((4/3.0) * pi * (r*r*r))
+print(f"VOLUME = {volume:.3f}")
